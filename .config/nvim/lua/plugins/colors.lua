55eff2fb02a5335b357b3594edb2c807efe82ce6
zhavelka <zhavelka@idc.com>
2024-11-21 16:09:00 +0100

    add gruvbox-material

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,80 +1,95 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "frappe",
-        transparent_background = true,
-        integrations = {
-          treesitter = true,
-          telescope = true,
-          mason = true,
-          cmp = true,
-        },
-      })
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      require("tokyonight").setup({
-        style = "storm",
-      })
-      -- vim.cmd.colorscheme("tokyonight")
-    end,
-  },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        -- disable_background = true,
-        dark_variant = "moon", -- auto,  main / moon / dawn
-        styles = {
-          italic = false,
-        },
-        enable = {
-          terminal = true,
-        },
-      })
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "frappe",
+                transparent_background = true,
+                integrations = {
+                    treesitter = true,
+                    telescope = true,
+                    mason = true,
+                    cmp = true,
+                },
+            })
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            require("tokyonight").setup({
+                style = "storm",
+            })
+            -- vim.cmd.colorscheme("tokyonight")
+        end,
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                -- disable_background = true,
+                dark_variant = "moon", -- auto,  main / moon / dawn
+                styles = {
+                    italic = false,
+                },
+                enable = {
+                    terminal = true,
+                },
+            })
 
-      -- vim.cmd.colorscheme("rose-pine")
-    end,
-  },
-  {
-    "morhetz/gruvbox",
-    config = function()
-      vim.o.background = "dark" -- light / dark
-      vim.cmd.colorscheme("gruvbox")
-    end,
-  },
-  {
-    "xiantang/darcula-dark.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
+            -- vim.cmd.colorscheme("rose-pine")
+        end,
     },
-  },
-  {
-    "briones-gabriel/darcula-solid.nvim",
-    dependencies = {
-      "rktjmp/lush.nvim",
+    -- {
+    --   "morhetz/gruvbox",
+    --   config = function()
+    --     vim.o.background = "dark" -- light / dark
+    --     vim.cmd.colorscheme("gruvbox")
+    --   end,
+    -- },
+    {
+        "xiantang/darcula-dark.nvim",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
     },
-  },
-  {
-    "shaunsingh/nord.nvim",
-    config = function()
-      vim.g.nord_contrast = true
-      vim.g.nord_borders = false
-      vim.g.nord_disable_background = false
-      vim.g.nord_italic = false
-      vim.g.nord_uniform_diff_background = true
-      vim.g.nord_bold = false
-    end,
-  },
+    {
+        "briones-gabriel/darcula-solid.nvim",
+        dependencies = {
+            "rktjmp/lush.nvim",
+        },
+    },
+    {
+        "shaunsingh/nord.nvim",
+        config = function()
+            vim.g.nord_contrast = true
+            vim.g.nord_borders = false
+            vim.g.nord_disable_background = false
+            vim.g.nord_italic = false
+            vim.g.nord_uniform_diff_background = true
+            vim.g.nord_bold = false
+        end,
+    },
+    {
+        "sainnhe/gruvbox-material",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.gruvbox_material_enable_italic = true
+            vim.g.gruvbox_material_background = "medium" -- hard | medium | soft
+            vim.g.gruvbox_material_foreground = "mix"   -- material | mix | original
+            vim.g.gruvbox_material_transparent_background = 0 -- 0 | 1 | 2
+            vim.g.gruvbox_material_ui_contrast = "high" -- "high" | "low"
+            vim.cmd.colorscheme("gruvbox-material")
+        end,
+    },
 }
